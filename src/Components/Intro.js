8abfe1e248157a53965543ad101f10eb003efa56
Brett Westerlund <drbretto82@gmail.com>
2021-03-25 10:39:08 -0400

    updated message

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -5,26 +5,36 @@ export default class Intro extends React.Component {
     return (
       <section id="intro" className="dark window">
         <p>
-          Welcome! This website is under construction. <br />
+          Welcome! This website is under construction. Please forgive the
+          general "geocities" vibe. I am applying for nuts and bolts work at
+          this time, not design. I assure you, I can duplicate any layout
+          required. :) <br />
           <br /> Above is a brief introduction. Just a brief backstory and
           preview of my general vibe.
+          <br />
           <br />
           Below is a collection of projects with links to source code for
           review.
           <br />
           <br /> While I am just looking for my start in this particular field,
-          I have been committed to this path for years. I realized I had not
-          lived up to the expectations I set as a kid, given the talents I had
-          demonstrated. <br />
-          <br />I took responsibility for my choices and committed to turning my
-          life round. I faced myself head on. Tackled my bad habits and
-          attitudes one-by-one. Quit smoking. Quit drinking. Ate vegetables.
-          Lost weight. Rebuilt my credit. Bought a house. <br />
-          <br />I took the long way 'round. But what I lack in experience, I
-          make up for in *experience*. If you represent a comfortable working
-          environment that values and nurtures continued development, I would
-          give as much passion and commitment to my work as I do my personal
-          growth. I'd be an ally, not just an employee.
+          I have been committed to this path for years. I took responsibility
+          for my choices and committed to turning my life round. I faced myself
+          head on. Tackled my bad habits and attitudes one-by-one. Quit smoking.
+          Quit drinking. Ate vegetables. Lost weight. Rebuilt my credit. Bought
+          a house. <br />
+          <br />
+          Now, I am putting the same effort into realizing my potential. I am
+          done skating by and wasting my mind. I have always understood tech
+          intuitively. I was meant for this work. I could find a bug in a
+          language I've never learned, written in wingdings because I have a
+          deep intuitive understanding of the underlying patterns. If your
+          company is the right environment, I am a moldable ball of raging
+          passion and untapped talent ready to be unleashed.
+          <br />
+          <br />I am best suited as a full-stack generalist rather than a front
+          or back end specialist, but the only thing I can't do is give birth.
+          And honestly, if I was motivated enough, I'd find a way to do that
+          too.
         </p>
       </section>
     );
